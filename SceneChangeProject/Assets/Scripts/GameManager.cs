1ec0633d7f35f9f78d55d2104e1e2ca695e7d2e3
Masayoshi Iraha <urasho6.12@gmail.com>
2021-04-27 23:12:15 +0900

    gameoverコミット

---
+++ SceneChangeProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject gameOverUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOverUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameOver();
+    }
+
+    public void GameOver()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            gameOverUI.SetActive(true);
+        }
+
+    }
+}
